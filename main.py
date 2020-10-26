579e67f8c7bce0ead5eb27f8380ae3cdcaeddec6
Sean Hofer <Sean@Seans-MBP.local>
2020-10-25 17:25:14 -0700

    minor changes - use origin instead of referer - adjust spacing - secrets

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import base64
-import os
 import smtplib
 import ssl
 from email.mime.multipart import MIMEMultipart
@@ -7,81 +6,84 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from google.cloud import secretmanager
 
+RECIPIENT = "me@seanhofer.com"
 PROJECT_ID = "coherent-coder-193013"
 
-USER_SECRET = f"projects/{PROJECT_ID}/secrets/seanbot-user/versions/latest"
-PASS_SECRET = f"projects/{PROJECT_ID}/secrets/seanbot-pass/versions/latest"
-DOMAIN_SECRET = f"projects/{PROJECT_ID}/secrets/contact-form-allowed-domains/versions/latest"
-
-RECIPIENT = "me@seanhofer.com"
-
 def contact(request):
-    """Handles Contact Form requests.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text.
-    """
-    client = secretmanager.SecretManagerServiceClient()
+  """Handles Contact Form requests.
 
-    u_encoded = client.access_secret_version(request={"name": USER_SECRET})
-    p_encoded = client.access_secret_version(request={"name": PASS_SECRET})
-    d_encoded = client.access_secret_version(request={"name": DOMAIN_SECRET})
+  Args:
+    request (flask.Request): HTTP request object.
+  Returns:
+    The response text.
+  """
+  client = secretmanager.SecretManagerServiceClient()
 
-    USER = u_encoded.payload.data.decode("UTF-8")
-    PASS = p_encoded.payload.data.decode("UTF-8")
-    ALLOWED_DOMAINS = d_encoded.payload.data.decode("UTF-8")
+  u_encoded = client.access_secret_version(request={"name": get_secret_path("seanbot-user")})
+  p_encoded = client.access_secret_version(request={"name": get_secret_path("seanbot-pass")})
+  d_encoded = client.access_secret_version(request={"name": get_secret_path("contact-form-allowed-domains")})
+  s_encoded = client.access_secret_version(request={"name": get_secret_path("smtp-domain")})
+  sp_encoded = client.access_secret_version(request={"name": get_secret_path("smtp-port")})
 
-    headers = {}
-    if request.headers['referer'] in ALLOWED_DOMAINS:
-        headers = {
-            'Access-Control-Allow-Origin': request.headers['referer'][:-1]
-        }
+  USER = u_encoded.payload.data.decode("UTF-8")
+  PASS = p_encoded.payload.data.decode("UTF-8")
+  ALLOWED_DOMAINS = d_encoded.payload.data.decode("UTF-8")
+  SMTP_DOMAIN = s_encoded.payload.data.decode("UTF-8")
+  SMTP_PORT = sp_encoded.payload.data.decode("UTF-8")
 
-    fields = {}
-    data = request.form.to_dict()
-    for field in data:
-        fields[field] = data[field]
+  headers = {}
+  if request.headers['origin'] in ALLOWED_DOMAINS:
+    headers = {
+      'Access-Control-Allow-Origin': request.headers['origin']
+    }
 
-    if not has_required_fields(fields):
-        return (f'Missing required fields. Include name email and message.', 400, headers)
+  fields = {}
+  data = request.form.to_dict()
+  for field in data:
+    fields[field] = data[field]
 
+  if not has_required_fields(fields):
+    return (f'Missing required fields. Include name email and message.', 400, headers)
+
+  try:
+    msg = MIMEMultipart()
+    msg['Subject'] = f'Message from {fields["name"]} via seanhofer.com!'
+    msg['From'] = f'"{fields["name"]}" {fields["email"]}'
+    msg['To'] = RECIPIENT
+    msg.add_header('reply-to', fields["email"])
+    msg.attach(MIMEText(fields["message"], 'plain'))
+  except Exception as e:
+    print("Error constructing message fields: {0}".format(str(e)))
+    return ("Error constructing message fields: {0}".format(str(e)), 500, headers)
+
+  if "attachments" in fields:
     try:
-        msg = MIMEMultipart()
-        msg['Subject'] = f'Message from {fields["name"]} via seanhofer.com!'
-        msg['From'] = f'"{fields["name"]}" {fields["email"]}'
-        msg['To'] = RECIPIENT
-        msg.add_header('reply-to', fields["email"])
-        msg.attach(MIMEText(fields["message"], 'plain'))
-    except Exception as e:
-        print("Error constructing message fields: {0}".format(str(e)))
-        return ("Error constructing message fields: {0}".format(str(e)), 500, headers)
+      b64_files = fields["attachments"].split(',')
+      file_names = fields["attachment_names"].split(',')
+      for index, b64_file in enumerate(b64_files):
+        bytes_file = base64.b64decode(b64_file)
+        part = MIMEApplication(bytes_file, Name=file_names[index])
+        part['Content-Disposition'] = f'attachment; filename="{file_names[index]}"'
+        msg.attach(part)
+    except:
+      print("Error attaching attachments. names: {0} attachments: {1}".format(
+        file_names, b64_files))
+      return ("Error attaching attachments. names: {0} attachments: {1}".format(file_names, b64_files), 500, headers)
 
-    if "attachments" in fields:
-        try:
-            b64_files = fields["attachments"].split(',')
-            file_names = fields["attachment_names"].split(',')
-            for index, b64_file in enumerate(b64_files):
-                bytes_file = base64.b64decode(b64_file)
-                part = MIMEApplication(bytes_file, Name=file_names[index])
-                part['Content-Disposition'] = f'attachment; filename="{file_names[index]}"'
-                msg.attach(part)
-        except:
-            print("Error attaching attachments. names: {0} attachments: {1}".format(file_names, b64_files))
-            return ("Error attaching attachments. names: {0} attachments: {1}".format(file_names, b64_files), 500, headers)
+  try:
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(SMTP_DOMAIN, SMTP_PORT, context=context) as server:
+      server.login(USER, PASS)
+      server.sendmail(fields["email"], RECIPIENT, msg.as_string())
+  except Exception as e:
+    print("Error sending email: {0}".format(str(e)))
+    return ("Error sending email: {0}".format(str(e)), 500, headers)
 
-    try:
-        context = ssl.create_default_context()
-
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-            server.login(USER, PASS)
-            server.sendmail(fields["email"], RECIPIENT, msg.as_string())
-    except Exception as e:
-        print("Error sending email: {0}".format(str(e)))
-        return ("Error sending email: {0}".format(str(e)), 500, headers)
-
-    return (f'success: "{msg.as_string()}"', 200, headers)
+  return (f'success: "{msg.as_string()}"', 200, headers)
 
 
 def has_required_fields(fields):
-    return 'name' in fields and 'email' in fields and 'message' in fields
+  return 'name' in fields and 'email' in fields and 'message' in fields
+
+def get_secret_path(secret_id):
+  return f"projects/{PROJECT_ID}/secrets/{secret_id}/versions/latest"
